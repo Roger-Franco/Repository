71902433512eace9589686b5c63c040b8c699727
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-12 13:47:30 -0400

    Implementando funções de botão "open, closed and all" para as issues

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,26 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import { Container, Owner, Loading, BackButton, IssuesList, PageActions } from './styles';
+import React, { useState, useEffect } from 'react';
+import { Container, Owner, Loading, BackButton, IssuesList, PageActions, FilterList } from './styles';
 import api from '../../services/api'
-import {FaArrowLeft} from 'react-icons/fa';
+import { FaArrowLeft } from 'react-icons/fa';
 
 
-export default function Repositorio({match}) {
+export default function Repositorio({ match }) {
   const [repositorio, setRepositorio] = useState({});
   const [issues, setIssues] = useState([]);
-  const [ loading, setLoading] = useState(true);
-  const [ page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [filters, setFilters] = useState([
+    { state: 'all', label: 'Todas', active: true },
+    { state: 'open', label: 'Abertas', active: false },
+    { state: 'closed', label: 'Fechadas', active: false },
+  ]);
+  const [filterIndex, setFilterIndex] = useState(0);
 
-  useEffect(() =>{
+  useEffect(() => {
 
 
-    async function load(){
+    async function load() {
       const nomeRepo = decodeURIComponent(match.params.repositorio);
 
-      const [ repositorioData, issuesData] = await Promise.all([
+      const [repositorioData, issuesData] = await Promise.all([
         api.get(`/repos/${nomeRepo}`),
         api.get(`/repos/${nomeRepo}/issues`, {
           params: {
-            state: 'open',
+            state: filters.filter(f => f.active).state,
             per_page: 5
           }
         })
@@ -35,16 +41,16 @@ export default function Repositorio({match}) {
 
     load();
 
-  }, [match.params.repositorio]);
+  }, [filters, match.params.repositorio]);
 
   useEffect(() => {
 
-    async function loadIssue(){
+    async function loadIssue() {
       const nomeRepo = decodeURIComponent(match.params.repositorio);
 
       const response = await api.get(`/repos/${nomeRepo}/issues`, {
-        params:{
-          state: 'open',
+        params: {
+          state: filters[filterIndex].state, 
           page,
           per_page: 5,
         },
@@ -54,17 +60,22 @@ export default function Repositorio({match}) {
 
     loadIssue();
 
-  }, [match.params.repositorio, page])
+  }, [filterIndex, filters, match.params.repositorio, page])
 
   function handlePage(action) {
-    setPage( action === 'back' ? page -1 : page +1)
+    setPage(action === 'back' ? page - 1 : page + 1)
   }
 
-  if(loading) {
-    return(
-    <Loading >
-      <h1>Carregando...</h1>
-    </Loading>
+  function handleFilter(index){
+    setFilterIndex(index);
+    console.log(setFilterIndex);
+  }  
+
+  if (loading) {
+    return (
+      <Loading >
+        <h1>Carregando...</h1>
+      </Loading>
     )
   }
 
@@ -74,47 +85,61 @@ export default function Repositorio({match}) {
         <FaArrowLeft color='#000' size={30} />
       </BackButton>
       <Owner>
-        <img 
-        src={repositorio.owner.avatar_url} 
-        alt={repositorio.owner.login}
+        <img
+          src={repositorio.owner.avatar_url}
+          alt={repositorio.owner.login}
         />
         <h1>{repositorio.name}</h1>
         <p>{repositorio.description}</p>
       </Owner>
-      
-    <IssuesList>
-      {issues.map(issue => (
-        <li key={String(issue.id)}>
-          <img src={issue.user.avatar_url} alt={issue.user.login} />
-          
-        <div>
-          <strong>
-            <a href={issue.html_url}>{issue.title}</a>
 
-            {issue.labels.map(label => (
-              <span key={String(label.id)}>{label.name}</span>
-            ))}
-          </strong>
-          <p>{issue.user.login}</p>
-        </div>
+      <FilterList active={filterIndex} >
+        {filters.map((filter, index) => (
+          <button
+            type="button"
+            key={filter.label}
+            onClick={() => handleFilter(index)}
+          >
+            {filter.label}
+          </button>
 
-        </li>
-      ))}
-    </IssuesList>
+        ))}
 
-    <PageActions>
-      <button 
-      type="button" 
-      onClick={()=> handlePage('back')}
-      disabled={page < 2}
-      >
-        Voltar
+      </FilterList>
+
+      <IssuesList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+          </li>
+        ))}
+      </IssuesList>
+
+      <PageActions>
+        <button
+          type="button"
+          onClick={() => handlePage('back')}
+          disabled={page < 2}
+        >
+          Voltar
         </button>
 
-      <button type="button" onClick={()=> handlePage('next')}>
-        Proxima
+        <button type="button" onClick={() => handlePage('next')}>
+          Proxima
         </button>
-    </PageActions>
+      </PageActions>
 
     </Container>
   );
